a423f373e2f598aed3f0e6ed0d58d77742c47e27
Fernando <asialer05@hotmail.com>
2025-01-16 16:01:39 -0500

    proyecto base

---
+++ src/main/java/com/fernando/ms/gateway/app/twitter_clone_gateway_server/TwitterCloneGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.gateway.app.twitter_clone_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/gateway/app/twitter_clone_gateway_server/TwitterCloneGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.gateway.app.twitter_clone_gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
